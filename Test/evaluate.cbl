40f6c87446e8145b41f3a96f0fdc8dbe1f33a71e
dan <dandan682@outlook.com>
2022-07-02 10:34:56 -0500

    update evaulate

--- Test/evaluate.cbl
+++ Test/evaluate.cbl
@@ -5,10 +5,13 @@
        01  STUDENT-NAME               PIC X(20).
        01  STUDENT-MAARK-1            PIC 999.
        01  STUDENT-MAARK-2            PIC 999.
+       01  A                          PIC 99.
        PROCEDURE DIVISION.
-           DISPLAY 'ENTER STUDENT DETAIL'
+           DISPLAY 'ENTER STUDENT NAME'
            ACCEPT STUDENT-NAME
+           DISPLAY 'GRADE 1:'
            ACCEPT STUDENT-MAARK-1
+           DISPLAY 'GRADE 2:'
            ACCEPT STUDENT-MAARK-2
            EVALUATE (STUDENT-MAARK-1 + STUDENT-MAARK-2) / 2
               WHEN 40 THRU 49.9 DISPLAY 'THIRD CLASS'
@@ -16,5 +19,17 @@
               WHEN 60 THRU 69.9 DISPLAY 'UPPER SECOND'
               WHEN 70 THRU 100  DISPLAY 'FIRST CLASS'
               WHEN OTHER        DISPLAY 'PROG OR DATA ERROR!'
-           END-EVALUATE.
+           END-EVALUATE
+           DISPLAY 'VALUE OF A:'
+           ACCEPT A
+           EVALUATE TRUE 
+              WHEN A > 10
+                 DISPLAY 'A IS GREATER THAN 10'
+              WHEN A > 5
+                 DISPLAY 'A IS GRATER THAN 5 BUT LESS THAN 10'
+              WHEN A > 0
+                 DISPLAY 'A IS GREATER THAH 0 BUT LES THAN 5'
+              WHEN OTHER
+                 DISPLAY 'IINVALID VALUE OF A'
+           END-EVALUATE 
            GOBACK.
