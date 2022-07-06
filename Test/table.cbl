66e01bee1d047c000c75eff015797e1d34fbf6ad
dan <dandan682@outlook.com>
2022-07-05 23:53:53 -0500

    update table

--- Test/table.cbl
+++ Test/table.cbl
@@ -7,7 +7,8 @@
               10 STORE-LOCATION    PIC 9.
               10 SALES             OCCURS 4 TIMES.
                  15 STORE-SALES    PIC 9(5).
-       01  COUNTER                 PIC 9 VALUE 1.
+       01  COUNTER-I               PIC 9 VALUE 1.
+       01  COUNTER-J               PIC 9 VALUE 1.
        PROCEDURE DIVISION.
            MOVE 300 TO STORE-SALES (1 1)
            MOVE 400 TO STORE-SALES (1 2)
@@ -17,19 +18,17 @@
            MOVE 530 TO STORE-SALES (2 2)
            MOVE 550 TO STORE-SALES (2 3)
            MOVE 560 TO STORE-SALES (2 4)
-           MOVE 560 TO STORE-SALES (3 1)
-           MOVE 560 TO STORE-SALES (3 2)
-           MOVE 560 TO STORE-SALES (3 3)
-           MOVE 560 TO STORE-SALES (3 4)
+           MOVE 660 TO STORE-SALES (3 1)
+           MOVE 680 TO STORE-SALES (3 2)
+           MOVE 630 TO STORE-SALES (3 3)
+           MOVE 670 TO STORE-SALES (3 4)
       *
-           PERFORM UNTIL COUNTER >= 5
-              DISPLAY STORE-SALES (1 COUNTER)
-              ADD 1 TO COUNTER
-           END-PERFORM
-           MOVE 1 TO COUNTER
-           PERFORM UNTIL COUNTER >= 5
-              DISPLAY STORE-SALES (2 COUNTER)
-              ADD 1 TO COUNTER
+           PERFORM UNTIL COUNT-I >= 4
+              PERFORM UNTIL COUNTER >= 5
+                 DISPLAY STORE-SALES (COUNTER-I  COUNTER-J)
+                 ADD 1 TO COUNTER-J
+              END-PERFORM
+              ADD 1 TO COUNTER-I
            END-PERFORM
            GOBACK.
            
