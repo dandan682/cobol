06bb1153e0a2eee8f19a5f49164e96664d7c32e7
daniel castro <dandan682@outlook.com>
2022-07-27 18:30:34 -0500

    update ejercicios

--- Ejercicios/eval_set.cbl
+++ Ejercicios/eval_set.cbl
@@ -9,7 +9,7 @@
        01 TESTNUMBER   PIC X.
           88 ISPRIME   VALUE '1', '3', '5', '7'.
           88 ISODD     VALUE '1', '3', '5', '7', '9'.
-          88 ISEVEN    VALUE '2', '4', '6', '8'.
+          88 ISEVEN    VALUE '2', '6', '8'.
           88 LESSTHAN5 VALUE '1' THRU '4'.
           88 ANUMBER   VALUE '0' THRU '9'.
        PROCEDURE DIVISION.
@@ -22,6 +22,7 @@
            END-IF 
            DISPLAY 'VOTE: ' CANVOTEFLAG
            DISPLAY 'ENTER SINGLE NUMBER OR X TO EXIT'
+           ACCEPT TESTNUMBER 
            PERFORM UNTIL NOT ANUMBER 
               EVALUATE TRUE 
                  WHEN ISPRIME   DISPLAY 'PRIME'
