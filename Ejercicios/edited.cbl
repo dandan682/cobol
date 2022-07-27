5194fed771036b11800261b7ba3b54956932e81e
daniel castro <dandan682@outlook.com>
2022-07-26 23:53:02 -0500

    update ejercicios

--- Ejercicios/edited.cbl
+++ Ejercicios/edited.cbl
@@ -4,6 +4,6 @@
        WORKING-STORAGE SECTION.
        01  STARTNUM    PIC 9(8)V99 VALUE 00001123.55.  
        01  NOZERO      PIC ZZZZZZZ9.99.
-       01
+       01  
        PROCEDURE DIVISION.
            GOBACK.
