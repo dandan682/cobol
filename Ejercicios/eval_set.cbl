a4a0512723f2905e54f6052487a3d044728cd62a
daniel castro <dandan682@outlook.com>
2022-07-27 21:10:17 -0500

    update ejercicios

--- Ejercicios/eval_set.cbl
+++ Ejercicios/eval_set.cbl
@@ -12,8 +12,13 @@
           88 ISEVEN    VALUE '2', '6', '8'.
           88 LESSTHAN5 VALUE '1' THRU '4'.
           88 ANUMBER   VALUE '0' THRU '9'.
+       01 EMPLOYEE-CODE PIC X.
+          88 MALE      VALUE '1'.
+          88 FEMALE    VALUE '2'.
+          88 ERROR-SEX-CODE VALUE LOW-VALUES THRU ZERO 
+                        '3' THRU HIGH-VALUES. 
        PROCEDURE DIVISION.
-           DISPLAY 'ENTER AGE: ' WITH NO ADVANCING 
+           DISPLAY 'ENTER AGE TO CHECK VOTING: ' WITH NO ADVANCING 
            ACCEPT AGE
            IF AGE > 18 THEN 
               SET CANVOTE TO TRUE
@@ -21,6 +26,8 @@
               SET CANTVOTE TO TRUE
            END-IF 
            DISPLAY 'VOTE: ' CANVOTEFLAG
+           SET ISEVEN TO TRUE
+           DISPLAY 'TEST NUMBER: ' TESTNUMBER 
            DISPLAY 'ENTER SINGLE NUMBER OR X TO EXIT'
            ACCEPT TESTNUMBER 
            PERFORM UNTIL NOT ANUMBER 
@@ -33,4 +40,12 @@
               END-EVALUATE 
               ACCEPT TESTNUMBER 
            END-PERFORM   
+           DISPLAY 'ENTER SEXODE 1 OR 2'
+           ACCEPT EMPLOYEE-CODE.
+           EVALUATE TRUE
+              WHEN MALE DISPLAY 'MALE'
+              WHEN FEMALE DISPLAY 'FEMALE'
+              WHEN ERROR-SEX-CODE DISPLAY 'ERROR SEX CODE'
+              WHEN OTHER DISPLAY 'DEFAULT CONDITION'
+           END-EVALUATE
            GOBACK.
