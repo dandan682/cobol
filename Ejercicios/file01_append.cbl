2bf3ff614d2f2f7bbcffb0febffe42076aff283c
daniel castro <dandan682@outlook.com>
2022-08-05 23:28:47 -0500

    update ejercicios

---
+++ Ejercicios/file01_append.cbl
@@ -0,0 +1,35 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID.     FILE02.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION. 
+       FILE-CONTROL. 
+           SELECT CUSTOMERFILE ASSIGN TO "CUSTOMER.DAT"
+              ORGANIZATION IS LINE SEQUENTIAL
+              ACCESS IS SEQUENTIAL. 
+       DATA DIVISION. 
+       FILE SECTION. 
+       FD CUSTOMERFILE.
+       01 CUSTOMERDATA.
+           02 IDNUM                PIC 9(5).
+           02 CUSTNAME.
+              03 FIRSTNAME         PIC X(15). 
+              03 LASTNAME          PIC X(15).  
+       WORKING-STORAGE SECTION. 
+       01 WS-CUSTOMERDATA.
+           02 WS-IDNUM             PIC 9(5).
+           02 WS-CUSTNAME.
+              03 WS-FIRSTNAME      PIC X(15). 
+              03 WS-LASTNAME       PIC X(15). 
+       PROCEDURE DIVISION.
+           OPEN EXTEND CUSTOMERFILE 
+           DISPLAY 'CUSTOMER ID: ' WITH NO ADVANCING 
+           ACCEPT IDNUM 
+           DISPLAY 'CUSTOMER FIRST NAME: ' WITH NO ADVANCING 
+           ACCEPT FIRSTNAME 
+           DISPLAY 'CUSTOMER LAST NAME: ' LASTNAME 
+           ACCEPT LASTNAME 
+           WRITE CUSTOMERDATA
+           END-WRITE
+           CLOSE CUSTOMERFILE 
+           GOBACK.
+       END PROGRAM FILE02.
