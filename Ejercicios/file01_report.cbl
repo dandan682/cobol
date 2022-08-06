36cbccd476f58d36b2108009383cd3155f187b2e
daniel castro <dandan682@outlook.com>
2022-08-06 00:00:30 -0500

    update ejercicios

--- Ejercicios/file01_report.cbl
+++ Ejercicios/file01_report.cbl
@@ -1,31 +1,41 @@
        IDENTIFICATION DIVISION. 
        PROGRAM-ID.     FILE04.
+      ***************************************
        ENVIRONMENT DIVISION.
+      ***************************************
        INPUT-OUTPUT SECTION. 
+      ***************************************
        FILE-CONTROL. 
-           SELECT CUSTOMERREPORT ADDIGN TO "CUSTOMER.RPT"
+           SELECT CUSTOMER-REPORT ASSIGN TO "CUSTOMER.RPT"
               ORGANIZATION IS LINE SEQUENTIAL.
-           SELECT CUSTOMERFILE ASSIGN TO "CUSTOMER.DAT"
+           SELECT CUSTOMER-FILE ASSIGN TO "CUSTOMER.DAT"
               ORGANIZATION IS LINE SEQUENTIAL.
+      ***************************************      
        DATA DIVISION. 
+      ***************************************
        FILE SECTION. 
-       FD CUSTOMERREPORT.
-       01 PRINTLINE                PIC X(44).
-       FD CUSTOMERFILE.
+      ***************************************
+       FD CUSTOMER-REPORT.
+       01 PRINT-LINE               PIC X(44).
+      ***************************************
+       FD CUSTOMER-FILE.
        01 CUSTOMERDATA.
            02 IDNUM                PIC 9(5).
            02 CUSTNAME.
               03 FIRSTNAME         PIC X(15). 
               03 LASTNAME          PIC X(15).  
            88 WS-EOF               VALUE HIGH-VALUE.
+      **************************************
        WORKING-STORAGE SECTION. 
-       
-       01 WS-CUSTOMER.
-           02 WS-IDNUM             PIC 9(5).
-           02 WS-CUSTNAME.
-              03 WS-FIRSTNAME      PIC X(15). 
-              03 WS-LASTNAME       PIC X(15). 
-       01 WS-EOF                   PIC A(1).
+       01 PAGE-HEADING.
+          02 FILLER                PIC X(13) VALUE 'CUSTOMER LIST'.
+       01 PAGE-FOOTING.
+          02 FILLER                PIC X(15) VALUE SPACE.
+          02 FILLER                PIC X(17) VALUE 'PAGE: '.
+          02 PRN-PAGENUM PIC Z9.
+       01 HEADER                   PIC X(36) VALUE 
+             'IDNUM         FIRST NAME   LAST NAME'
+      ***************************************
        PROCEDURE DIVISION.
            OPEN INPUT CUSTOMERFILE 
            PERFORM UNTIL WS-EOF = 'Y' 
