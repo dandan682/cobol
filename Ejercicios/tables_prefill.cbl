187f9955a7f6c782a0dee41103141696b4d48cbd
daniel castro <dandan682@outlook.com>
2022-08-09 16:16:15 -0500

    update ejercicios

--- Ejercicios/tables_prefill.cbl
+++ Ejercicios/tables_prefill.cbl
@@ -6,28 +6,28 @@
        WORKING-STORAGE SECTION.
        01 PROD-TABLE.
           02 PROD-DATA.
-             03 FILLER PIC X(8) VALUE 'RED SML'.
+             03 FILLER PIC X(8) VALUE 'RED  SML'.
              03 FILLER PIC X(8) VALUE 'BLUE SML'.
              03 FILLER PIC X(8) VALUE 'GREENSML'.
           02 FILLER REDEFINES PROD-DATA.
              03 SHIRT OCCURS 3 TIMES.
-                04 PRODNAME  PIC X(5).
-                04 PROSIZES  PIC A OCCURS 3 TIMES.
+                04 PRODNAME              PIC X(5).
+                04 PROSIZES              PIC A OCCURS 3 TIMES.
       *****
        01 CHANGEME.
-          02 TEXTNUM         PIC X(6).
-          02 FLOATNUM REDEFINES TEXTNUM PIC 9(4)V99.
+          02 TEXTNUM                     PIC X(6).
+          02 FLOATNUM REDEFINES TEXTNUM  PIC 9(4)V99.
       *****
-       01 STRNUM             PIC X(7).
+       01 STRNUM                         PIC X(7).
       *****
        01 SPLITNUM.
-          02 WNUM            PIC 9(4) VALUE ZERO.
-          02 FNUM            PIC 99 VALUE ZERO.     
-       01 FLNUM REDEFINES SPLITNUM PIC 9999V99.
-       01 DOLLARNUM          PIC $$,$$9.99.    
+          02 WNUM                        PIC 9(4) VALUE ZERO.
+          02 FNUM                        PIC 99   VALUE ZERO.     
+       01 FLNUM REDEFINES SPLITNUM       PIC 9999V99.
+       01 DOLLARNUM                      PIC $$,$$9.99.    
       *****
        PROCEDURE DIVISION.
-           DISPLAY SHIRT(1)
+           DISPLAY '*'SHIRT(1)'*'PRODNAME(1)'*'PROSIZES(1 1)'*'
            MOVE '123456' TO TEXTNUM
            DISPLAY FLOATNUM 
            DISPLAY 'ENTER A FLOAT: ' WITH NO ADVANCING.
