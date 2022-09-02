3247be5b1b932a9d9a4cae459cceee1ca5f8f40e
daniel bernardo castro <dandan682@outlook.com>
2022-09-02 10:30:42 -0500

    update ejercicios

---
+++ Ejercicios/operaciones.cbl
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.     OPERA.
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION. 
+       01 WS-NUM1         PIC 99 VALUE 10.
+       01 WS-NUM2         PIC 99 VALUE 65.
+       01 WS-RESUL        PIC 99 VALUE ZEROES.
+       PROCEDURE DIVISION.
+       
+           GOBACK.
