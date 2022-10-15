8f2ff2aa598b0d3f3c90ae0e55667f145504f0a7
daniel bernardo castro <dandan682@outlook.com>
2022-10-15 13:00:19 -0500

    update ejercicios

---
+++ Ejercicios/cics01.cbl
@@ -0,0 +1,25 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.     PRUEBAC1.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-SALIDA-L     PIC S9(4) COMP.
+       01 WS-SALIDA       PIC X(10).
+       PROCEDURE DIVISION.
+           PERFORM 1000-INICIO.
+           PERFORM 2000-PROCESO.
+           PERFORM 3000-FIN.
+           GOBACK.
+       1000-INICIO.
+      * INICIALIZAMOS LA VARIABLE DE SALIDA.
+           MOVE 'HOLA MUNDO' TO WS-SALIDA.
+           MOVE 10           TO WS-SALIDA-L.
+       2000-PROCESO.
+      * ENVIAMOS LA VARIABLE AL CICS.
+           EXEC CICS SEND TEXT
+              FROM (WS-SALIDA)
+              LENGTH (WS-SALIDA-L)               
+           END-EXEC.
+       3000-FIN.
+      * DEVOLVEMOS EL CONTROL AL CICS
+           EXEC CICS RETURN END-EXEC.
+           
